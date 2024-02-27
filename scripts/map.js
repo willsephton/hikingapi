b06ffebc71dd9ff8c2b6bef5df183e509076fee3
William Sephton <willsephton1234@gmail.com>
2024-02-27 01:59:32 +0000

    adds markers to the map

--- scripts/map.js
+++ scripts/map.js
@@ -1,14 +1,24 @@
 function initMap() {
     // Initialize the map
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 35.825588, lng: -78.738358 },
+        center: { lat: 50.935020, lng: -1.396370 },
         zoom: 8
     });
 
-    // Example marker
-    const marker = new google.maps.Marker({
-        position: { lat: 35.825588, lng: -78.738358 },
-        map: map,
-        title: 'Marker'
-    });
+    fetch('/obstructions')
+        .then(response => response.json())
+        .then(obstructions => {
+            // Iterate over the obstructions data
+            obstructions.forEach(obstruction => {
+                // Create marker for each obstruction
+                const marker = new google.maps.Marker({
+                    position: { lat: obstruction.latitude, lng: obstruction.longitude },
+                    map: map,
+                    title: obstruction.type // You can set the title to whatever you want to display when the marker is clicked
+                });
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching obstructions:', error);
+        });
 }
