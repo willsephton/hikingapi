d3ae81ae9176cbd60fa54fcc4f8e47cc0d98dc01
William Sephton <willsephton1234@gmail.com>
2024-03-16 00:29:45 +0000

    PopUp 14 Debugging 4

--- scripts/map.js
+++ scripts/map.js
@@ -20,11 +20,14 @@ function initMap() {
                     title: obstruction.type
                 });
 
+
+                formattedDate = formatDate(obstruction.date)
                 // Add click event listener to each marker
                 marker.addListener('click', () => {
                     // Set the content of the info window to display the information of the obstruction
-                    infoWindow.setContent(`<div><strong>Type:</strong> ${obstruction.type}</div>
-                                           <div><strong>Date:</strong> ${obstruction.date}</div>
+                    infoWindow.setContent(`<div><strong>ID:</strong> ${obstruction.id}</div>
+                                            <div><strong>Type:</strong> ${obstruction.type}</div>
+                                           <div><strong>Date:</strong> ${formattedDate}</div>
                                            <div><strong>Trail:</strong> ${obstruction.trail}</div>
                                            <div><strong>Username:</strong> ${obstruction.username}</div>
                                            <div><strong>Description:</strong> ${obstruction.description}</div>
@@ -38,4 +41,17 @@ function initMap() {
         .catch(error => {
             console.error('Error fetching obstructions:', error);
         });
+}
+
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+    console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`)
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
