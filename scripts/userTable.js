57842b428c00c8d1af2a3e82cca8c4af8f2fcc5a
William Sephton <willsephton1234@gmail.com>
2024-03-21 11:54:24 +0000

    Users table 2

--- scripts/userTable.js
+++ scripts/userTable.js
@@ -61,6 +61,18 @@ function fetchAndDisplayUsers() {
         });
 }
 
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+    console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`)
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 function displayEditPopup(user) {
     // Display the edit popup with pre-filled data from the user object
     const popupHtml = `
